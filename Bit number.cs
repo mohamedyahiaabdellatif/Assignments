b5490383409aac650768c5b8ff29809f90ff1fff
mohamedyahiaabdellatif <33098570+mohamedyahiaabdellatif@users.noreply.github.com>
2017-10-30 22:16:00 +0200

    Add files via upload

---
+++ Bit number.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueOfBit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number,n;
+            string p;
+
+            Console.WriteLine("Enter a number :");
+            number = int.Parse(Console.ReadLine());
+
+           p = Convert.ToString(number, 2);
+            Console.WriteLine("The Number in Binary :\n" + p);
+
+            Console.WriteLine("Enter a specific Position n :");
+          n= int.Parse(Console.ReadLine());
+            Console.WriteLine("The value of Bit at Position  " + p + " is:\n" + p[n - 1]);
+            Console.ReadLine();
+        }
+    }
+}

97cc822c0cf3c5b99ca4bacd132a6714f06aef9b
mohamedyahiaabdellatif <33098570+mohamedyahiaabdellatif@users.noreply.github.com>
2017-10-30 22:11:42 +0200

    Add files via upload

---
+++ Hex to Decimal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string Num;
+                Console.WriteLine("Enter a Hexadecimal Number :");
+               Num = Console.ReadLine();
+                int Output = int.Parse(Num, System.Globalization.NumberStyles.HexNumber);
+                Console.WriteLine("The Decimal value is " + Output);
+                Console.Read();
+            }
+        }
+    }
+
+   
+
